1938a9f2ca110e8f16e533b4bd3f958b89e4550d
Hemenguel bindi <nilafe4@gmail.com>
2023-05-25 18:00:46 +0500

    Изменение маршрутизации

--- blog/models.py
+++ blog/models.py
@@ -16,7 +16,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_post')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
@@ -38,5 +38,4 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return reverse('blog:post_detail', args=[self.id])
-    
     
--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,21 @@ from .models import Post
 
 def post_list(request):
     post = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': post})
+    return render(request, 
+                  'blog/post/list.html', 
+                  {'posts': post})
 
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
-    return render(request, 'blog/post/list.html', {'posts': post})
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, 
+                             status=Post.Status.PUBLISHED, 
+                             slug=post, 
+                             publish__year=year, 
+                             publish__month=month, 
+                             publish__day=day,
+                             )
+    return render(request, 
+                  'blog/post/list.html', 
+                  {'posts': post})
 
 
