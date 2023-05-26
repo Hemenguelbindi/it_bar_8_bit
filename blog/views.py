05ea2a54c6b4252d6f11812cb53c3bc793813eac
Hemenguel bindi <nilafe4@gmail.com>
2023-05-26 16:47:21 +0500

    Изменения шаблона создан шаблон для пагинации

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Post
 
 
 def post_list(request):
     post = Post.published.all()
+    paginator = Paginator(post, 3)
+    page_number = request.GET.get('page', 1)
+    posts= paginator.page(page_number)
     return render(request, 
                   'blog/post/list.html', 
-                  {'posts': post})
+                  {'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
